d04f3fa9a738cb138c0e8bda867f448091c4a341
Davy <100140271+DavyR01@users.noreply.github.com>
2025-08-10 15:26:24 +0200

    MyProjects : complete datas

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,7 +4,7 @@ export type ProjectCardProps = {
   index: number;
   name: string;
   descriptionKey: string;
-  tags: { name: string; color: string }[];
+  tags: { name: string; color?: string }[];
   image: string;
   sourceWeb?: string;
   sourceGithub?: string;
