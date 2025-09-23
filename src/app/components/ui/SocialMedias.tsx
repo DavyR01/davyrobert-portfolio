cd65b7c580d00bdfc34c05641dcae2c7d5235e8f
Davy <100140271+DavyR01@users.noreply.github.com>
2025-09-23 12:23:26 +0200

    Presentation : - Change label title and modif layout - Create reusable component SocialMedias

---
+++ src/app/components/ui/SocialMedias.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
+
+const SocialMedias = () => {
+    return (
+        <>
+            <a
+                href="https://github.com/davyR01"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-[--primary-color] text-5xl sm:text-4xl hover:scale-110 transition-transform"
+            >
+                <FaGithub />
+            </a>
+            <a
+                href="https://www.linkedin.com/in/davy-robert"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-[--primary-color] text-5xl sm:text-4xl  hover:scale-110 transition-transform"
+            >
+                <FaLinkedin />
+            </a>
+            <a
+                href="mailto:contact@davyrobert.fr"
+                className="text-[--primary-color] text-5xl sm:text-4xl  hover:scale-110 transition-transform"
+            >
+                <MdEmail />
+            </a>
+        </>
+    )
+}
+
+export default SocialMedias
