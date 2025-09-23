7794e5bdc0274ceae56491b1d9335a712d232f68
Davy <100140271+DavyR01@users.noreply.github.com>
2025-09-23 15:07:48 +0200

    Presentation : Update CV portfolio

--- src/app/components/Presentation.tsx
+++ src/app/components/Presentation.tsx
@@ -262,14 +262,14 @@ const Presentation = () => {
                <div className="flex lg:flex-col flex-wrap w-full xl:w-[66%] gap-4 sm:flex-nowrap sm:items-left mb-4">
                   <Button
                      variant="primary"
-                     href="/download/CV_davy_robert_2025_V6.pdf"
+                     href="/download/CV_Davy_ROBERT_DEVELOPPEUR_2025.pdf"
                      download
                   >
                      {t('cvStandard')}
                   </Button>
                   <Button
                      variant="secondary"
-                     href="/download/CV_davy_robert_détails_du_parcours_2025.pdf"
+                     href="/download/CV_DETAILLE_Davy_ROBERT_DEVELOPPEUR_2025.pdf"
                      download
                      withShineEffect
                   >
